6723b74d9a03754be83780a00229e670ae1b373e
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-21 13:17:49 +0100

    1

---
+++ HomeWork41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i == 0; i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 2);
+
+        }
+    }
+}
+int[,] matrix = new int[1, 8];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
